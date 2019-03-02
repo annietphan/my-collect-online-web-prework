593188d5d073deab87bf607ed4c4368ccad75029
Annie Phan <anniephan.atp@gmail.com>
2019-03-02 22:50:53 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,1 +1,13 @@
-def my_collect
+def my_collect(array)
+  counter = 0
+  new_array = []
+  while i < array.length
+    new_array << yield(array[counter])
+    counter += 1
+  end
+  new_array
+end
+
+  
+
+  
